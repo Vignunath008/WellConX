72b915b126f940e1cd5af78b601192d064ed2b64
Vignunath008 <vignunathareddy@gmail.com>
2025-07-08 23:05:04 +0530

    Fix unused imports in EHR.tsx

--- src/pages/EHR.tsx
+++ src/pages/EHR.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { 
   User, 
@@ -12,17 +12,11 @@ import {
   Upload,
   Search,
   Filter,
-  Star,
-  Clock,
   AlertCircle,
   CheckCircle,
   Eye,
   Edit,
-  Trash2,
   Plus,
-  Tag,
-  Shield,
-  Activity,
   Heart,
   Stethoscope,
   Clipboard
